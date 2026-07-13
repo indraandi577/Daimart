4373efb862296f1e6b08047fb69e9c2047a600e7
indraandi577 <indraandi577@gmail.com>
2026-07-13 12:30:29 +0700

    bismillah fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
 import type { NextConfig } from "next";
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Turbopack config (Next.js 16 default)
+  turbopack: {},
 };
 
-export default withPWA(nextConfig);
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+})(nextConfig);
